5cf4b74ac7d267b1608b7bfeec4b67244f5d7441
Selva Balasingam <selva.balasingam@gmail.com>
2015-06-27 15:55:22 -0400

    initial commit

---
+++ apps/app.js
@@ -0,0 +1,33 @@
+// On submit (to get request 'Pixiwoo')
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+});
+
+// To get request
+function getRequest(searchTerm){
+  var params = {
+    s: searchTerm,
+    r: 'json',
+    part:'snippet',
+    key: 'AIzaSyBLO_74kbdhUfBOFDlKDEdCGKOlfd71dww'
+  	};
+  	url = 'https://www.googleapis.com/youtube/v3/search';
+
+  	$.getJSON(url, params, function(data){
+    showResults(data.Search);
+ 	});
+}
+
+// To show results
+function showResults(results){
+	var html = "";
+	$.each(results, function(index,value){
+	    html += '<p>' + value.Title + '</p>';
+	    console.log(value.Title);
+	});
+	$('#search-results').html(html);
+	}
